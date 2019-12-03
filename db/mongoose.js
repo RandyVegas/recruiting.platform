f9f89346c01eb66abfdcf7b3e41f462c9ab9336a
i.tsarkov <i.tsarkov@tinkoff.ru>
2019-12-03 13:18:50 +0300

    change mongo settings

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recruiting-platform', {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    useCreateIndex: true
 }).catch(err => {
         console.log ('ERROR connecting to: ' + err);
 });
