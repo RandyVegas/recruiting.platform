913ee8ea8c619de6881ddc3948b64d626c6b349d
i.tsarkov <i.tsarkov@tinkoff.ru>
2019-12-03 13:22:33 +0300

    change port settings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./db/mongoose');
 const vacancyRouter = require('./routers/vacancies');
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(vacancyRouter);
