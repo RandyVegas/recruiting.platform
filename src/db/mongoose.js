0fb0eaf9c900a8b7f75f5fe7068704f92d3407fe
Igor <slabiydoter@gmail.com>
2019-12-12 22:25:26 +0300

    change mongo settings

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOLAB_PURPLE_URI || 'mongodb://127.0.0.1:27017/recruiting-platform', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recruiting-platform', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 }).catch(err => {
-    console.log(process.env.MONGOLAB_PURPLE_URI);
         console.log ('ERROR connecting to: ' + err);
 });
