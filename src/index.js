4391cfebc37835dce1a019d3f15eb6680490d859
Igor <slabiydoter@gmail.com>
2019-12-12 22:19:22 +0300

    add view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('./db/mongoose');
 const cors = require('cors');
+const hbs = require('hbs');
+const path = require('path');
 const userRouter = require('./routers/user');
 const vacancyRouter = require('./routers/vacancies');
 const eventsRouter = require('./routers/events');
@@ -8,12 +10,31 @@ const eventsRouter = require('./routers/events');
 const app = express();
 const port = process.env.Port || 3000;
 
+const publickDirectory = path.join(__dirname, './public');
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
 app.use(cors());
 
 app.use(express.json());
 app.use(userRouter);
 app.use(vacancyRouter);
 app.use(eventsRouter);
+
+app.use(express.static(publickDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'recruiting-platform',
+        name: 'Igor'
+    });
+});
+
+app.use(express.json());
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
